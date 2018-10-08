c0400fc6a48c2b18cd1c0cdb8655de68f0fc006a
roh-7 <rohit.6233@outlook.com>
2018-10-08 23:06:34 +0530

    first

---
+++ app/src/main/java/com/example/fitnesstracker/FitnessApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnesstracker;
+
+import android.app.Application;
+
+public class FitnessApplication extends Application
+{
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		
+	}
+}
